f91dc89ba241e32b9855288b953b1951bbdf86e3
mariamargaret <44648153+mariamargaret@users.noreply.github.com>
2024-09-04 15:14:24 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,7 @@ output "vpc_endpoint_2_EW" {
 output "tgw-id" {
   value = aws_ec2_transit_gateway.sec-tgw.id
 }
+
+output "service_name" {
+  value = aws_vpc_endpoint_service.sec-gwlb_endpoint_service.service_name
+}
